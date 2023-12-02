6bbec558d75ddc0c4fb10f10127c1275c8cd7a96
Shane Brewer <theproros@gmail.com>
2023-12-02 16:52:16 +1300

    Finished part one, working on part 2

--- advent_day1_q1/src/main/scala/Main.scala
+++ advent_day1_q1/src/main/scala/Main.scala
@@ -1,5 +1,20 @@
-@main def hello: Unit =
-  println("Hello world!")
-  println(msg)
+import scala.io.Source
 
-def msg = "I was compiled by Scala 3. :)"
+@main def parse(input: String): Unit = {
+  val count: Int = Source
+    .fromFile(input)
+    .getLines
+    .map(firstAndLastInt)
+    .reduceLeft(_ + _)
+  println(count)
+}
+
+def firstAndLastInt(line: String): Int = {
+  val firstIndex = line.indexWhere(_.isDigit)
+  val firstChar: Char = line(firstIndex)
+  val lastIndex = line.lastIndexWhere(_.isDigit)
+  val lastChar: Char = line(lastIndex)
+
+  (s"${firstChar}${lastChar}").toInt
+}
+
