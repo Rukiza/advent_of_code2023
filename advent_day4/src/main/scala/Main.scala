64827f1305d23fac9b8b1904628df4832dfa2e50
Shane Brewer <theproros@gmail.com>
2023-12-04 18:46:19 +1300

    Add day 4 part 1 complete and input

--- advent_day4/src/main/scala/Main.scala
+++ advent_day4/src/main/scala/Main.scala
@@ -1,6 +1,58 @@
-
+import scala.collection.mutable.ArrayBuffer
 
 @main def parse(input: String): Unit = {
   val path: os.Path = os.pwd / input
   val lines: Seq[String] = os.read.lines(path)
+
+  println(lines
+    .map(parseGameLine)
+    .reduce(_ + _))
 }
+
+def parseGameLine(line: String): Int = {
+
+  val game: String = line.split(":")(1)
+  println(game)
+  val split: Array[String] = game.split('|')
+  split.map(println(_))
+  val (winnersStr, oursStr) = (split(0).trim, split(1).trim)
+
+  println(winnersStr)
+  println(oursStr)
+
+  val winner = winnersStr.split(' ')
+    .filterNot(_ == "")
+    .map(_.trim)
+    .map(_.toInt)
+  val ours = oursStr.split(' ')
+    .filterNot(_ == "")
+    .map(_.trim)
+    .map(_.toInt)
+
+  calculatePoints(winner, ours) match {
+    case x if x.isEmpty => 0
+    case x => x.reduce(_ * _)
+  }
+
+}
+
+def calculatePoints(winner: Array[Int], ours: Array[Int]): ArrayBuffer[Int] = {
+
+  val array: ArrayBuffer[Int] = ArrayBuffer[Int]()
+  var first = false
+
+  for (value <- ours) do {
+    println("value: "+value)
+    if (!first && winner.contains(value)) {
+      array.addOne(1)
+      first = true
+    } 
+    else if (winner.contains(value)) {      
+      array.addOne(2)
+    }
+  }
+
+  array
+
+}
+
