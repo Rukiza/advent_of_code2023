48b9683bf531f5f870a305698e8f5d3c605c8368
Shane Brewer <theproros@gmail.com>
2023-12-04 20:14:09 +1300

    Completed day 4 part 2

--- advent_day4/src/main/scala/Main.scala
+++ advent_day4/src/main/scala/Main.scala
@@ -1,25 +1,52 @@
 import scala.collection.mutable.ArrayBuffer
+import scala.collection.mutable.Map
 
 @main def parse(input: String): Unit = {
   val path: os.Path = os.pwd / input
   val lines: Seq[String] = os.read.lines(path)
-
-  println(lines
-    .map(parseGameLine)
-    .reduce(_ + _))
+  println(parseLines(lines))
 }
 
-def parseGameLine(line: String): Int = {
+def parseLines(lines: Seq[String]): Int = {
+  var map: Map[Int, Int] = Map()
+  var keys: ArrayBuffer[Int] = ArrayBuffer()
+  for (line <- lines) do  {
+    val (card, matches) = parseCard(line)
+    keys.addOne(card) 
+    val extraGames = map.get(card) match {
+      case None => 0
+      case Some(value) => value
+    }
+    println(s"card: ${card} extraGames: ${extraGames}")
+    for (i <- card + 1 to card + matches) do {
+      var temp = map.get(i) match {
+        case None => (i, 1 + extraGames)
+        case Some(value) => {
+          (i, value + 1 + extraGames)
+        }
+      }
+      map.addOne(temp)
+    }
+    map.addOne((card, extraGames + 1))
+  }
+  keys.map(map.get(_))
+   .flatMap(x => x)
+   .reduce(_ + _)
+}
 
-  val game: String = line.split(":")(1)
-  println(game)
+def parseCard(line: String): (Int, Int) = {
+  val splitString: Array[String] = line.split(":")
+  val (cardStr, game) = (splitString(0), splitString(1))
+  val card: Int = cardStr.split(" ")
+    .filterNot(_ == "")(1)
+    .trim
+    .toInt
+  (card, parseGameLine(game))
+}
+
+def parseGameLine(game: String): Int = {
   val split: Array[String] = game.split('|')
-  split.map(println(_))
   val (winnersStr, oursStr) = (split(0).trim, split(1).trim)
-
-  println(winnersStr)
-  println(oursStr)
-
   val winner = winnersStr.split(' ')
     .filterNot(_ == "")
     .map(_.trim)
@@ -28,31 +55,20 @@ def parseGameLine(line: String): Int = {
     .filterNot(_ == "")
     .map(_.trim)
     .map(_.toInt)
-
-  calculatePoints(winner, ours) match {
+  calculateWin(winner, ours) match {
     case x if x.isEmpty => 0
-    case x => x.reduce(_ * _)
+    case x => x.reduce(_ + _)
   }
-
 }
 
-def calculatePoints(winner: Array[Int], ours: Array[Int]): ArrayBuffer[Int] = {
-
+def calculateWin(winner: Array[Int], ours: Array[Int]): ArrayBuffer[Int] = {
   val array: ArrayBuffer[Int] = ArrayBuffer[Int]()
   var first = false
-
   for (value <- ours) do {
-    println("value: "+value)
-    if (!first && winner.contains(value)) {
+    if (winner.contains(value)) {      
       array.addOne(1)
-      first = true
-    } 
-    else if (winner.contains(value)) {      
-      array.addOne(2)
     }
   }
-
   array
-
 }
 
