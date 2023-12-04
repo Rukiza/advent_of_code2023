48506ed18c508d706146e144ba1c9636a029679e
Shane Brewer <theproros@gmail.com>
2023-12-04 20:19:18 +1300

    Initalize file loading

--- advent_day5/src/main/scala/Main.scala
+++ advent_day5/src/main/scala/Main.scala
@@ -1,5 +1,9 @@
-@main def hello: Unit =
-  println("Hello world!")
-  println(msg)
+import scala.collection.mutable.ArrayBuffer
+import scala.collection.mutable.Map
 
-def msg = "I was compiled by Scala 3. :)"
+@main def parse(input: String): Unit = {
+  val path: os.Path = os.pwd / input
+  val lines: Seq[String] = os.read.lines(path)
+  println("")
+}
+
