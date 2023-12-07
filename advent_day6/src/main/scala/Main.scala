d7db3a829281a9ba44b68c777a0f248df43d8c7c
Shane Brewer <theproros@gmail.com>
2023-12-07 19:32:44 +1300

    Add part 1 day 6

--- advent_day6/src/main/scala/Main.scala
+++ advent_day6/src/main/scala/Main.scala
@@ -7,3 +7,42 @@ import scala.collection.mutable.Map
   println(parseLines(lines))
 }
 
+def parseLines(lines: Seq[String]): Int = {
+  val times: List[Int] = parseString(lines, 0)
+  val distances: List[Int] = parseString(lines, 1)
+
+  println(times)
+  println(distances)
+  var total = 1
+  for i <- 0 to times.length - 1 do {
+    var sum = 0
+    for t <- 0 to times(i) - 1 do {
+      if (t * (times(i) - t) > distances(i)) {
+        sum += 1
+      }
+    }
+    println(sum)
+    total = total * sum
+  }
+  total
+}
+
+def parseString(lines: Seq[String], index: Int): List[Int] = {
+  val parsed: List[Int] = lines(index)
+    .split(':')(1)
+    .split(' ')
+    .filterNot(_ == "")
+    .map(_.toInt)
+    .toList
+
+  parsed
+}
+
+
+// Understanding.. 
+// min is 
+// T D 
+// floor(D / T) + 1 recuse until D / T grather than Prev floor(D/T) + 1
+//
+//
+
