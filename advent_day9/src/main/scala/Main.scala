2e7801f3b7eea97fdc57495357c48f6de4c0a4ee
Shane Brewer <theproros@gmail.com>
2023-12-09 20:52:15 +1300

    Add input and test

--- advent_day9/src/main/scala/Main.scala
+++ advent_day9/src/main/scala/Main.scala
@@ -4,7 +4,11 @@ import scala.collection.mutable.Map
 @main def parse(input: String): Unit = {
   val path: os.Path = os.pwd / input
   val lines: Seq[String] = os.read.lines(path)
-  println(parseLines(lines))
+  // println(parseLines(lines))
 }
 
+def parseLines(lines: Seq[String]): Long = {
+
+  0L
+}
 
